20bfdb51baf99ecc9be5d0db742381b075db395f
Eric Schall <61393763+laggerstatten@users.noreply.github.com>
2024-02-28 15:01:41 -0600

    change nav bar

--- components/main_footer.js
+++ components/main_footer.js
@@ -1,26 +1,26 @@
 class MainFooter extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-        <footer class="pt-6">
-          <div class="container">
-            <!-- Divider -->
-            <div class="divider opacity-1"></div>
-            <!-- Footer copyright START -->
-            <div class="row align-items-center justify-content-md-between py-4">
-              <div class="col-md-6">
-                <div class="text-center text-md-start">©2024 <a href="https://www.humboldtgeoscience.com/" target="_blank">Eric Schall</a>. All rights reserved
-                </div>
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <footer class="pt-6">
+        <div class="container">
+          <!-- Divider -->
+          <div class="divider opacity-1"></div>
+          <!-- Footer copyright START -->
+          <div class="row align-items-center justify-content-md-between py-4">
+            <div class="col-md-6">
+              <div class="text-center text-md-start">©2024 <a href="https://www.humboldtgeoscience.com/" target="_blank">Eric Schall</a>. All rights reserved
               </div>
             </div>
-            <!-- Footer copyright END -->
           </div>
-        </footer>
+          <!-- Footer copyright END -->
+        </div>
+      </footer>
       `;
-    }
   }
-  
-  customElements.define('main-footer-component', MainFooter);
+}
+
+customElements.define('main-footer-component', MainFooter);
